7ce2e97386f46a85ce313da52725be9ebb70ace5
Joel Shapiro <joelshapiro@Joels-M2A.local>
2022-09-20 21:44:12 -0700

    updates for the spooky season

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import {
   notifyIftttForFailure,
   notifyIftttForOpenings,
 } from "./result-helpers";
-import {EateryNames} from "./url-consts";
+import {EateryNames, QUERY_DELAY_DURATION} from "./url-consts";
 
 const start = async (lookups: lookupDataDefinition[]) => {
   for (let i = 0; i < lookups.length; i++) {
@@ -26,7 +26,7 @@ const start = async (lookups: lookupDataDefinition[]) => {
     }
 
     if (lookups[i + 1]) {
-      await delayInMS(1400);
+      await delayInMS(QUERY_DELAY_DURATION);
     }
   }
   return true;
@@ -54,11 +54,57 @@ const generateLookupsForSpring2002 = (eatery: EateryNames) => {
   ];
 };
 
-const lookupsToRun: lookupDataDefinition[] = [
+const lookupsToRunForSpring2002: lookupDataDefinition[] = [
   ...generateLookupsForSpring2002(EateryNames.Oga),
   ...generateLookupsForSpring2002(EateryNames.Lamplight),
   ...generateLookupsForSpring2002(EateryNames.CarthayLounge),
   ...generateLookupsForSpring2002(EateryNames.TraderSams),
 ];
 
-start(lookupsToRun);
+type DateAndPersonCountDataDefinition = {date: string, count: number}
+
+const generateLookupsForEateryOnDates = (
+  eatery: EateryNames,
+  datesAndCountsData: DateAndPersonCountDataDefinition[]
+) => {
+  const lookupDataForSpookdays2022: lookupDataDefinition[] = datesAndCountsData.map(({date, count}) => {
+    return generateLookupData(date, eatery, count);
+  });
+  return lookupDataForSpookdays2022;
+};
+
+const eateriesForSpookdays2022: EateryNames[] = [
+  EateryNames.Oga,
+  EateryNames.Lamplight,
+  // EateryNames.CarthayLounge,
+  EateryNames.TraderSams,
+  EateryNames.BlueBayou,
+  EateryNames.BlueBayouFantasmic,
+  EateryNames.RiverBelleTerraceFantasmic,
+  // EateryNames.CafeOrleans, turned off nearly immediately on 9/14
+  EateryNames.CarthayCircleRestaurant,
+  // EateryNames.TomorrowlandSkylineLoungeExperience,
+  // EateryNames.WineCountryTrattoria, turned off nearly immediately on 9/14
+  // EateryNames.WorldOfColorDessertParty, turned off nearly immediately on 9/14
+];
+
+const TWO_PERSON_COUNT = 2;
+const FOUR_PERSON_COUNT = 4;
+
+const dataForSpookdays2022: DateAndPersonCountDataDefinition[] = [
+  { date: "10-06-2022", count: TWO_PERSON_COUNT },
+  { date: "10-07-2022", count: TWO_PERSON_COUNT },
+  { date: "10-08-2022", count: FOUR_PERSON_COUNT },
+  { date: "10-09-2022", count: TWO_PERSON_COUNT },
+];
+
+const lookupsToRunForSpookdays2022: lookupDataDefinition[] = [];
+eateriesForSpookdays2022.forEach(
+  eatery => {
+    lookupsToRunForSpookdays2022.push(...generateLookupsForEateryOnDates(
+      eatery,
+      dataForSpookdays2022,
+    ));
+  })
+
+start(lookupsToRunForSpookdays2022);
--- src/url-consts.ts
+++ src/url-consts.ts
@@ -8,6 +8,8 @@ export const IFTTT_KEY = "felvPa6aBfh_DOZ6eoPgDj-8U5kVjNXoWh1lY11bWLM";
 
 export const IFTTT_KEY_FOR_KATIE = "chNWCbjcW9MN59AzhWUCnu";
 
+export const QUERY_DELAY_DURATION = 1400;
+
 export type IFTTTEventWebhookDefinition = {
   value1: string; // eatery
   value2: string; // mealtime
@@ -35,6 +37,14 @@ export enum EateryNames {
   Lamplight = "Lamplight Lounge",
   CarthayLounge = "Carthay Circle Lounge",
   TraderSams = "Trader Sams",
+  BlueBayou = "Blue Bayou",
+  BlueBayouFantasmic = "Blue Bayou (Fantasmic)",
+  RiverBelleTerraceFantasmic = "River Belle Terrace (Fantasmic)",
+  CafeOrleans = "Cafe Orleans",
+  CarthayCircleRestaurant = "Carthay Circle Restaurant",
+  TomorrowlandSkylineLoungeExperience = "Tomorrowland Skyline Lounge Experience",
+  WineCountryTrattoria = "Wine Country Trattoria",
+  WorldOfColorDessertParty = "World of Color Dessert Party",
 }
 
 export const EateryIDs = {
@@ -42,4 +52,12 @@ export const EateryIDs = {
   [EateryNames.Lamplight]: "126",
   [EateryNames.CarthayLounge]: "145",
   [EateryNames.TraderSams]: "151",
+  [EateryNames.BlueBayou]: "99",
+  [EateryNames.BlueBayouFantasmic]: "180",
+  [EateryNames.RiverBelleTerraceFantasmic]: "90",
+  [EateryNames.CafeOrleans]: "100",
+  [EateryNames.CarthayCircleRestaurant]: "101",
+  [EateryNames.TomorrowlandSkylineLoungeExperience]: "170",
+  [EateryNames.WineCountryTrattoria]: "110",
+  [EateryNames.WorldOfColorDessertParty]: "175",
 };
